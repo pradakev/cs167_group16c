2116809b721ed9c6632d4075c90d96b8bc4abade
Kevin Prada <pradakev@gmail.com>
2024-03-14 12:47:41 -0700

    Initial commit

---
+++ src/main/scala/edu/ucr/cs/cs167/group16/App.scala
@@ -0,0 +1,15 @@
+package edu.ucr.cs.cs167.group16
+
+/**
+ * @author ${Group 16 }
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
